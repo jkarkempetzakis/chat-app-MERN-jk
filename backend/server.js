87d1c9c9492500f44aa44d51860436af35ae371a
jkarkempetzakis <ikarkempetzakis.tech@gmail.com>
2024-03-15 21:58:19 +0200

    Fixed server path

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 //Package Imports
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 //File imports
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,6 +15,8 @@ import { app, server } from './socket/socket.js'
 
 //Variables
 
+//deployment variable -->absolute path to root
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 //environment variable config
@@ -25,6 +29,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
